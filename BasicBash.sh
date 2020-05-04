9b03a974e90f38c0e7841b80a54b8e6bc3014741
Francis Vachon <Francis Vachon>
2020-05-04 16:50:44 -0400

    conditionals

--- BasicBash.sh
+++ BasicBash.sh
@@ -23,36 +23,75 @@ HEADINGS: seems best to declare the headings early on, then  re-use the symbols.
 Summary of content 
 ==============================
 
-1 - IF / ELIF / ELSE
+1 - Variable, xpression evals
+++++++++++++++++++++++++++++++
 
-
-
-IF ELIF ELSE 
-+++++++++++++++++++++++++++++
-read input
-echo INPUT:
-$input
-
-
-
-
-
-
-
-
-
-
-
-
-
+2 - IF / ELIF / ELSE
++++++++++++++++++++++++
 
 
 
 The following line ends the comment section.
 COMMENTS
 
+<<'COMMENTS_varsxpress'
+
+Variables & xpression evals
++++++++++++++++++++++++++++++
+Demoing various syntaxes
+
+COMMENTS_varsxpress
+read -p "Some prompt as to what I expect you to enter. 
+Tell me your name:" name
+
+echo Hello World, $name!!!
+echo Comparing different expressions evaluations...
+
+echo "Text and variable in echo:"$name
+n=4
+
+echo "Xpression - \$(n+1). (doesn't work)"
+echo "Xpression - \$(\$(n+1)).(doesn't work)"
+echo "Xpression - ((n+1)). (just regular text)"
+echo "Xpression \$((n+1)) - $((n+1)). (proper arithmetic)"
+echo "End part 1"
+echo ""
+echo "-----------------------------"
+<<'CMTS_IF'
+
+IF - ELIF - ELSE
+++++++++++++++++++++++++++
+CMTS_IF
+
+read -p "If (0), elif (1), else(anything):" input
+
+a=0
+b=1
 
 
-read Please enter an input: $input 
+echo "Testing if & truth values"
 
-echo Hello World!!!
+if [[ input = $a  &&  0 = $a ]]
+then
+  echo "'[0]' is true"
+else
+  echo "'[0]' is false"
+fi
+if [ 1 ]
+then
+  echo "'[1]' is true"
+else
+  echo "'[1]' is false"
+fi
+
+if [[ $input -eq $a ]]
+  then
+  echo "Complex conditions: '[ cd 1 ] || [ cd2 ] '"
+  echo "If was selected (=0 or less than -99): $input"
+elif [ $input -eq $b ]
+  then
+    echo "Elif was selected: $input"
+else
+  echo "Else was selected: $input"
+fi
+
