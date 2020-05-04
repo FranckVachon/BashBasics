97ebea1b642e8cdfb46ab5bd0fb45d303a04cfbe
Francis Vachon <Francis Vachon>
2020-05-04 14:10:32 -0400

    first

---
+++ BasicBash.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+<<'COMMENTS'
+==============================
+1 - Using RST
+Trying to integrate it, at least just the very basics. Then include that everywhere.
+
+HEADINGS: seems best to declare the headings early on, then  re-use the symbols.
+==============================
+
+
+2 - Level 2 headings 
++++++++++++++++++++++++++++++
+
+
+3 - Level 3
+****************************
+
+4 - Lvl 4
+----------------------------
+
+
+Summary of content 
+==============================
+
+1 - IF / ELIF / ELSE
+
+
+
+IF ELIF ELSE 
++++++++++++++++++++++++++++++
+read input
+echo INPUT:
+$input
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+The following line ends the comment section.
+COMMENTS
+
+
+
+read Please enter an input: $input 
+
+echo Hello World!
